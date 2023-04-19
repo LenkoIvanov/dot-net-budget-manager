e0142a5122d2063686afdd3178b0288d848115d0
Lenko Ivanov <72096565+LenkoIvanov@users.noreply.github.com>
2023-04-19 19:12:05 +0300

    chore: reorder budget info page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,10 @@ import Head from "next/head";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-import { SingleBudgetPage } from "./single_budget_page/SingleBudgetPage";
 import { NewBudgetForm } from "@/shared_components/new_budget_form/NewBudgetForm";
 import { useState } from "react";
 import { NavBar } from "@/shared_components/nav_bar/NavBar";
+import { SingleBudgetPage } from "@/shared_components/single_budget_page/SingleBudgetPage";
 
 export default function Home() {
   const [isBudgetMenu, setIsBudgetMenu] = useState<Boolean>(true);
