b825ec2374c792e4bad29c8ea54dfd47a5adfcef
Lenko Ivanov <72096565+LenkoIvanov@users.noreply.github.com>
2023-04-19 23:33:48 +0300

    feat: add checkboxes to budget list item (no functionality yet)

--- src/shared_components/budget_list_item/BudgetListItem.tsx
+++ src/shared_components/budget_list_item/BudgetListItem.tsx
@@ -1,13 +1,28 @@
 import { MdDelete } from "react-icons/md";
 import { IBudgetListItemProps } from "./IBudgetListItemProps";
+import { Checkbox } from "primereact/checkbox";
 import styles from "./BudgetListItem.module.css";
+import { useState } from "react";
 
 export const BudgetListItem = (props: IBudgetListItemProps) => {
   const { name } = props;
+  const [checked, setChecked] = useState<boolean>(false);
+
+  const handleChange = () => {
+    setChecked(!checked);
+  };
 
   return (
     <div className={styles.listItemContainer}>
-      {name}
+      <span style={{ display: "flex", alignItems: "end" }}>
+        <input
+          type="checkbox"
+          onChange={handleChange}
+          checked={checked}
+          style={{ color: "steelblue", cursor: "pointer" }}
+        />
+        {name}
+      </span>
       <MdDelete style={{ cursor: "pointer" }} />
     </div>
   );
