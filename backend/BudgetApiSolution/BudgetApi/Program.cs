de12bafbe09565dec857d7bf2b43c9d4d02ca07c
LenkoIvanov17 <l.ivanov@blubito.com>
2023-05-04 17:43:11 +0300

    feat: made first fe request

--- backend/BudgetApiSolution/BudgetApi/Program.cs
+++ backend/BudgetApiSolution/BudgetApi/Program.cs
@@ -5,6 +5,23 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDBContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
+builder.Services.AddCors((options =>
+{
+    options.AddPolicy("Policy",
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:3000", "http://localhost:5018")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+}));
 var app = builder.Build();
 app.MapControllers();
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("Access-Control-Allow-Origin", "http://localhost:3000");
+    context.Response.Headers.Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    await next.Invoke();
+});
+app.UseCors();
 app.Run();
--- frontend/src/pages/main_app_page/MainAppPage.tsx
+++ frontend/src/pages/main_app_page/MainAppPage.tsx
@@ -1,10 +1,28 @@
 import { BudgetPage } from "@/shared_components/budget_page/BudgetPage";
 import { NewBudgetForm } from "@/shared_components/new_budget_form/NewBudgetForm";
 import { SingleBudgetPage } from "@/shared_components/single_budget_page/SingleBudgetPage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const MainAppPage = () => {
   const [isBudgetMenu, setIsBudgetMenu] = useState<Boolean>(true);
+
+  const fetchApi = async () => {
+    try {
+      const res = await fetch(`http://localhost:5018/api/currency`, {
+        method: "GET",
+        mode: "cors",
+        headers: { accept: "application/json" },
+      });
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  // useEffect(() => {
+
+  //   fetchApi();
+  // }, []);
 
   const togglePageView = (toggle: Boolean) => {
     setIsBudgetMenu(toggle);
@@ -17,6 +35,7 @@ export const MainAppPage = () => {
       ) : (
         <SingleBudgetPage toggleBudgetMenu={togglePageView} />
       )}
+      <button onClick={fetchApi}>bla</button>
     </>
   );
 };
